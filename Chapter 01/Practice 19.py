6273f6086da1ff728e5e77ac7b8897e57bdce54b
Karuperu <162394168+Karuperu@users.noreply.github.com>
2024-03-15 17:25:58 +0900

    Delete Chapter 01 directory

--- Chapter 01/Practice 19.py
+++
@@ -1,4 +0,0 @@
-def find_Mm (a):
-    return min(a), max(a)
-A=[1, 50, 10, 54, 37, 49, 31, 94, 50]
-print(find_Mm(A))
--- Chapter 01/Practice 8.py
+++
@@ -1,6 +0,0 @@
-a = int(input('a = ? '))
-b = int(input('b = ? '))
-for i in range(max(a,b),(a*b)+1):
-    if i % a == 0 and i % b == 0:
-        print(i)
-        break
