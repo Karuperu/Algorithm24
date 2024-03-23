551b3af261b4f55ba477ae8444b9d38ab64f936f
Karuperu <andra00772200@gmail.com>
2024-03-23 13:46:44 +0900

    Deleted File

--- Jamboard/Circulation Factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-num = factorial(int(input("팩토리얼을 구할 수 입력 : ")))
-print(num)
--- Jamboard/Repeat Factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    result = 1
-    for k in range(n, 0, -1):
-        result = result * k
-    return result
-num = factorial(int(input("팩토리얼을 구할 수 입력 : ")))
-print(num)
