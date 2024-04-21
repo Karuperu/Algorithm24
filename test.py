1ed1961f24315e2f48d83fe6e8ff8297c36e1a74
Karuperu <andra00772200@gmail.com>
2024-04-21 16:49:24 +0900

    test

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
-def binary_search_iter(A, key, low, high):
-    while(low <= high):
-        mid = (low + high) // 2
-        if key == A[mid]:
-            return mid
-        elif key > A[mid]:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return -1
+def quick_select(A, left, right, k):
+    pos = partition(A, left, right)
+    if(pos + 1 == left + k):
+        return A[pos]
+    elif (pos+1 > left):
+        return quick_select(A, left, pos-1, k)
+    else:
+        return quick_select(A, pos+1, right, k-(pos+1-left))
 
-listA = [1, 3, 8, 13, 13, 16, 21, 26, 27, 30, 33, 36, 39, 41, 44, 49]
-print("입력 리스트 =",listA)
-print("33 탐색(반복) -->", binary_search_iter(listA, 33, 0, len(listA)-1))
-print("32 탐색(반복) -->", binary_search_iter(listA, 32, 0, len(listA)-1))
+def partition(A, left, right):
+    low = left + 1
+    high = right
+    pivot = A[left]
+    while(low <= high):
+        while low <= right and A[low] <= pivot : low += 1
+        while high >= left and A[high] > pivot : high -= 1
+        if low < high:
+            A[low], A[high] = A[high], A[low]
+    A[left], A[high] = A[high], A[left]
+    return high
+
+array = [12, 3, 5, 7, 4, 19, 26, 23, 15]
+print("입력 리스트 =", array)
+n = len(array)
+print("[축소정복] 3번째 작은 수: ", quick_select(array, 0, n - 1, 3))
+print("[축소정복] 6번째 작은 수: ", quick_select(array, 0, n - 1, 6))
