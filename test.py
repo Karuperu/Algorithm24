a601c7d94cfd329a429b6ecee68357b5275fbccb
Karuperu <andra00772200@gmail.com>
2024-04-22 16:56:03 +0900

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ def quick_select(A, left, right, k):
     pos = partition(A, left, right)
     if(pos + 1 == left + k):
         return A[pos]
-    elif (pos+1 > left):
+    elif (pos+1 > left + k):
         return quick_select(A, left, pos-1, k)
     else:
         return quick_select(A, pos+1, right, k-(pos+1-left))
