9e92b44934fad3dde0cbd216d12bb912db3df95b
Karuperu <andra00772200@gmail.com>
2024-05-14 13:25:03 +0900

    Updated

--- test.py
+++ test.py
@@ -1,45 +1,29 @@
-import math
-def distance(p1, p2):
-    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+def merge_sort(A, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(A, left, mid)
+        merge_sort(A, mid + 1, right)
+        merge(A, left, mid, right)
 
-def closest_pair(p):
-    n = len(p)
-    mindist = float("inf")
-    for i in range(n-1):
-        for j in range(i+1, n):
-            dist = distance(p[i], p[j])
-            if dist < mindist:
-                mindist = dist
-    return mindist
-
-def strip_closest(P, d):
-    n = len(P)
-    d_min = d
-    P.sort(key = lambda point:point[1])
-    for i in range(n):
-        j = i + 1
-        while j < n and (P[j][1] - P[i][1]) < d_min:
-            dij = distance(P[i],P[j])
-            if dij < d_min:
-                d_min = dij
-            j += 1
-    return d_min
-
-def closest_pair_dist(P, n):
-    if n <= 3:
-        return closest_pair(P)
-    mid = n // 2
-    mid_x = P[mid][0]
-    dl = closest_pair_dist(P[:mid], mid)
-    dr = closest_pair_dist(P[mid:], n-mid)
-    d = min(dl, dr)
-    Pm = []
-    for i in range(n):
-        if abs(P[i][0] - mid_x) < d:
-            Pm.append(P[i])
-    ds = strip_closest(Pm, d)
-    return ds
-
-p = [(2,3),(12,30),(40,50),(5,1),(12,10),(3,4)]
-p.sort(key = lambda point:point[0])
-print("가장 가까운 두 점의 거리", closest_pair_dist(p, len(p)))
+def merge(A, left, mid, right):
+    k = left
+    i = left
+    j = mid + 1
+    while i <= mid and j <= right:
+        if A[i] <= A[j]:
+            sorted[k] = A[i]
+            i, k = i+1, k+1
+        else:
+            sorted[k] = A[j]
+            j, k = j+1, k+1
+    if i > mid:
+        sorted[k:k+right-j+1] = A[j:right+1]
+    else:
+        sorted[k:k+mid-i+1] = A[i:mid+1]
+    A[left:right+1] = sorted[left:right+1]
+    
+data = [5, 3, 8, 4, 9, 1, 6, 2, 7]
+sorted = [0] * len(data)
+print("Original :", data)
+merge_sort(data, 0, len(data)-1)
+print("MergeSort :", data)
