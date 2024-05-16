1b8c0ffd418330b712ce357ddc8db6fc88086c22
Karuperu <andra00772200@gmail.com>
2024-05-16 14:51:17 +0900

    Update

--- test.py
+++ test.py
@@ -1,29 +1,25 @@
-def merge_sort(A, left, right):
-    if left < right:
-        mid = (left + right) // 2
-        merge_sort(A, left, mid)
-        merge_sort(A, mid + 1, right)
-        merge(A, left, mid, right)
-
-def merge(A, left, mid, right):
-    k = left
-    i = left
-    j = mid + 1
-    while i <= mid and j <= right:
-        if A[i] <= A[j]:
-            sorted[k] = A[i]
-            i, k = i+1, k+1
-        else:
-            sorted[k] = A[j]
-            j, k = j+1, k+1
-    if i > mid:
-        sorted[k:k+right-j+1] = A[j:right+1]
-    else:
-        sorted[k:k+mid-i+1] = A[i:mid+1]
-    A[left:right+1] = sorted[left:right+1]
-    
-data = [5, 3, 8, 4, 9, 1, 6, 2, 7]
-sorted = [0] * len(data)
-print("Original :", data)
-merge_sort(data, 0, len(data)-1)
-print("MergeSort :", data)
+from queue import Queue
+def radix_sort(A):
+    queues = []
+    for i in range(BUCKETS):
+        queues.append(Queue())
+    n = len(A)
+    factor = 1
+    for d in range(DIGITS):
+        for i in range(n):
+            queues[(A[i]//factor)%BUCKETS].put(A[i])
+        j=0
+        for b in range(BUCKETS):
+            while not queues[b].empty():
+                A[j] = queues[b].get()
+                j+=1
+        factor *= BUCKETS
+        print("step", d+1, A)
+import random
+BUCKETS = 10
+DIGITS = 4
+data = []
+for i in range(10):
+    data.append(random.randint(1,9999))
+radix_sort(data)
+print("Radix:",data)
